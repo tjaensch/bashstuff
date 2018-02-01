522962d2fac0fb86e6ccaa719f049ee7af463fa5
Thomas Jaensch <thomas.jaensch@noaa.gov>
2018-02-01 19:27:02 +0000

    Added rename_with_fileidentifier.sh

---
+++ rename_with_fileidentifier.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+FILES=/nodc/users/tjaensch/geoportalScraper/collections/*
+
+for f in $FILES
+do
+  filename=$(basename $f)
+  fileidentifier=$(xml_grep 'gmd:fileIdentifier/gco:CharacterString' $f --text_only)
+  # remove colon from file name
+  fileidentifier=${fileidentifier//:}
+  # rename file with file identifier w/ colon removed
+  mv $f /nodc/users/tjaensch/geoportalScraper/collections/$fileidentifier.xml
+  
+  echo "$filename $fileidentifier"
+done
