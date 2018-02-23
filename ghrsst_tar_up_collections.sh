288eb993e90645f910403714a8b773337aad73a9
Thomas Jaensch <thomas.jaensch@noaa.gov>
2018-02-23 14:09:00 +0000

    Changed nodc/projects/satdata/Granule_OneStop to nodc/projects/metadata

--- ghrsst_tar_up_collections.sh
+++ ghrsst_tar_up_collections.sh
@@ -3,5 +3,5 @@ cd /nodc/web/data.nodc/htdocs/nodc/archive/metadata/test/granule/iso/ghrsst_new/
 for dir in */
 do
   base=$(basename "$dir")
-  tar -czf "${base}.tar.gz"  "/nodc/projects/satdata/Granule_OneStop/GHRSST/xml"
+  tar -czf "${base}.tar.gz"  "/nodc/projects/metadata/GHRSST/xml"
 done
